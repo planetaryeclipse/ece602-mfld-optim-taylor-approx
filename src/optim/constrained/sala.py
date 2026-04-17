b775b24a346987c10f4d5975d30227762a049c09
Samuel Street <planetaryeclipse@gmail.com>
2026-04-17 17:30:31 -0400

    Add tested rsqo algorithm

--- src/optim/constrained/sala.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-from optim.constrained.ralm import AugmentedLagrangian
-
-# implements the Safeguarded Augmented Lagrangian Algorithm (SALA) described in "Constraint Qualifications and Strong
-# Global Convergence Properties of an Augmented Lagrangian Method on Riemannian Manifolds"
-# NOTE: the definition of the augmented lagrangian in this paper is the same as that used to define RALM
