aaf0aaed3d63c30ff3d949b1575bcd8ba11ec73c
Samuel Street <planetaryeclipse@gmail.com>
2026-04-24 14:15:27 -0400

    Fix testing metrics

--- src/testing/testing_metrics.py
+++ src/testing/testing_metrics.py
@@ -16,9 +16,9 @@ def euclid_metric(x1, x2, x3, scaling: float):
 def scaled_metric(x1, x2, x3, scaling: float):
     # elements are assigned to this metric directly to preserve gradient history
     metric = torch.zeros((3, 3))
-    metric[0, 0] = (scaling * x1) ** 2 + 1.  # constant prevents degeneracy at origin
-    metric[1, 1] = (scaling * x2) ** 2 + 1.
-    metric[2, 2] = (scaling * x3) ** 2 + 1.
+    metric[0, 0] = (x1 / scaling) ** 2 + 1.  # constant prevents degeneracy at origin
+    metric[1, 1] = (x2 / scaling) ** 2 + 1.
+    metric[2, 2] = (x3 / scaling) ** 2 + 1.
 
     # pulls the metric "back" onto the scaled coordinate system
     factor = 1. / scaling ** 2
@@ -29,17 +29,17 @@ def scaled_metric(x1, x2, x3, scaling: float):
 
 def coupled_metric(x1, x2, x3, scaling: float):
     metric = torch.zeros((3, 3))
-    metric[0, 0] = (scaling * x1) ** 2 + 1.
-    metric[0, 1] = 0.5 * (scaling * x1) * (scaling * x2)
-    metric[0, 2] = 0.5 * (scaling * x1) * (scaling * x3)
+    metric[0, 0] = (x1 / scaling) ** 2 + 1.
+    metric[0, 1] = 0.5 * (x1 / scaling) * (x2 / scaling)
+    metric[0, 2] = 0.5 * (x1 / scaling) * (x3 / scaling)
 
-    metric[1, 0] = 0.5 * (scaling * x2) * (scaling * x1)
-    metric[1, 1] = (scaling * x2) ** 2 + 1.
-    metric[1, 2] = 0.5 * (scaling * x2) * (scaling * x3)
+    metric[1, 0] = 0.5 * (x2 / scaling) * (x1 / scaling)
+    metric[1, 1] = (x2 / scaling) ** 2 + 1.
+    metric[1, 2] = 0.5 * (x2 / scaling) * (x3 / scaling)
 
-    metric[2, 0] = 0.5 * (scaling * x3) * (scaling * x1)
-    metric[2, 1] = 0.5 * (scaling * x3) * (scaling * x2)
-    metric[2, 2] = (scaling * x3) ** 2 + 1.
+    metric[2, 0] = 0.5 * (x3 / scaling) * (x1 / scaling)
+    metric[2, 1] = 0.5 * (x3 / scaling) * (x2 / scaling)
+    metric[2, 2] = (x3 / scaling) ** 2 + 1.
 
     # pulls the metric "back" onto the scaled coordinate system
     factor = 1. / scaling ** 2
@@ -50,9 +50,9 @@ def coupled_metric(x1, x2, x3, scaling: float):
 
 def asymmetric_metric(x1, x2, x3, scaling: float):
     metric = torch.zeros((3, 3))
-    metric[0, 0] = (scaling * x1) ** 2 + 1.
-    metric[1, 1] = 0.5 * (x1 * scaling) ** 2 * (x2 * scaling) ** 2 + 1.
-    metric[2, 2] = 0.25 * (x1 * scaling) ** 2 * (x2 * scaling) ** 2 * (x3 * scaling) ** 2 + 1.
+    metric[0, 0] = (x1 / scaling) ** 2 + 1.
+    metric[1, 1] = 0.5 * (x1 / scaling) ** 2 * (x2 / scaling) ** 2 + 1.
+    metric[2, 2] = 0.25 * (x1 / scaling) ** 2 * (x2 / scaling) ** 2 * (x3 / scaling) ** 2 + 1.
 
     # pulls the metric "back" onto the scaled coordinate system
     factor = 1. / scaling ** 2
